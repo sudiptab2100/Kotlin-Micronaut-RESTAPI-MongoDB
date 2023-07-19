f1490506a0b394e7c3fac5153d61c5ded388c1ac
Sudipta Basak <sudiptab2100@gmail.com>
2023-07-19 20:45:26 +0530

    first commit - WORKING

---
+++ src/main/kotlin/example/micronaut/FruitController.kt
@@ -0,0 +1,27 @@
+package example.micronaut
+
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.HttpStatus.CONFLICT
+import io.micronaut.http.HttpStatus.CREATED
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import org.reactivestreams.Publisher
+import reactor.core.publisher.Mono
+import jakarta.validation.Valid
+
+@Controller("/fruits") // <1>
+open class FruitController(private val fruitService: FruitRepository) { // <2>
+
+    @Get("/all") // <3>
+    fun list(): Publisher<Fruit> = fruitService.list()
+
+    @Get("/filter/{fruitName}")
+    fun getById(fruitName: String) = fruitService.getByName(fruitName)
+
+    @Post("/add") // <4>
+    open fun save(@Valid fruit: Fruit): Mono<HttpStatus> { // <5>
+        return fruitService.save(fruit) // <6>
+                .map { added: Boolean -> if (added) CREATED else CONFLICT }
+    }
+}
